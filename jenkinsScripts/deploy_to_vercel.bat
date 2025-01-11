ee1d4816ff10400208b562a5fe260d7fae21a0f8
Rafajorda <rafajorgis@gmail.com>
2025-01-11 17:31:29 +0100

    Jenkinsfile30

--- jenkinsScripts/deploy_to_vercel.bat
+++ jenkinsScripts/deploy_to_vercel.bat
@@ -1,10 +1,7 @@
 @echo off
-npm install -g vercel
+
 REM Verificar si el token de Vercel está presente
 echo Token de Vercel: %VERCEL_TOKEN%
 
 REM Intentar hacer el deploy a Vercel
-vercel --prod --token %VERCEL_TOKEN%
-
-REM Confirmar la versión de Vercel después del deploy
-vercel --version
+vercel --prod --token %VERCEL_TOKEN% --yes
